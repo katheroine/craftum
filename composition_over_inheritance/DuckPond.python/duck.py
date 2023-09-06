e39a6ad259580c3d7ba829bf6a5e0f011537148b
katheroine <katheroine@gmail.com>
2023-09-06 11:28:09 +0200

    Python DuckPond: Duck implemented [#composition_over_inheritance]

--- composition_over_inheritance/DuckPond.python/duck.py
+++ composition_over_inheritance/DuckPond.python/duck.py
@@ -1,8 +1,11 @@
 class Duck:
     species = ""
 
-    def __init__(self, species):
-        self.species = species
-
     def get_species(self):
         return self.species
+
+    def quack(self):
+        print("Quack!")
+
+    def fly(self):
+        print("Weee!")
--- composition_over_inheritance/DuckPond.python/test_duck.py
+++ composition_over_inheritance/DuckPond.python/test_duck.py
@@ -1,17 +1,36 @@
 import unittest
 from duck import Duck
+import sys
+from io import StringIO
 
 class TestDuck(unittest.TestCase):
     def test(self):
-        duck = Duck("Mallard")
-        number = 100
+        duck = Duck()
         self.assertIsInstance(duck, Duck)
 
     def test_get_species(self):
-        expected_species = "xyz"
-        duck = Duck(expected_species)
+        expected_species = ""
+        duck = Duck()
         actual_species = duck.get_species()
         self.assertEqual(actual_species, expected_species)
+
+    def test_quack(self):
+        duck = Duck()
+        output = StringIO()
+        sys.stdout = output
+        duck.quack()
+        captured_output = output.getvalue()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output, "Quack!\n")
+
+    def test_fly(self):
+        duck = Duck()
+        output = StringIO()
+        sys.stdout = output
+        duck.fly()
+        captured_output = output.getvalue()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output, "Weee!\n")
 
 
 if __name__ == '__main__':
